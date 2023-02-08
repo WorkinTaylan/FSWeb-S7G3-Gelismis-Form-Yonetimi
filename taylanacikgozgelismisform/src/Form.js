dcbb6ff786e08d3b0825c59d77c10712b9ce665a
WorkinTaylan <acikgozta@gmail.com>
2023-02-09 00:55:53 +0300

    Form düzenlendi.Değerler ve eventler atandı.

--- taylanacikgozgelismisform/src/Form.js
+++ taylanacikgozgelismisform/src/Form.js
@@ -1,8 +1,31 @@
 import React, {useState} from 'react';
-
+import * as Yup from "yup"
 
 export default function Formolustur(){
 
+    const[formInfo, setFormInfo]=useState({
+        isim:"initalValue",
+        soyisim:"",
+        email:"",
+        password:"",
+        kosulKabul:false
+    })
+
+    const handleChange=event=>{
+        const {name, type, value, checked}=event.target;
+        const updatedInfo=type==='checkbox'?checked:value;
+        setFormInfo({...formInfo, [name]:updatedInfo})
+    }
+
+    const handleReset=()=>{
+        setFormInfo({
+            isim:"",
+            soyisim:"",
+            email:"",
+            password:"",
+            kosulKabul:false
+        })
+    }
 
 
     return (
@@ -13,30 +36,33 @@ export default function Formolustur(){
                 <legend style={{color:'white',fontFamily:"sans-serif", fontSize:"30px"}}>Yeni Kisi</legend>
                 <p>
                  <label>İsim Soyisim</label>
-                 <input type="text" id="isim" name="isim" value="isim"></input>
-                 <input type="text" id="soyisim" name="soyisim" value="soyisim"></input>
+                 <input type="text" id="name" name="isim" value={formInfo.isim} onChange={handleChange}></input>
+                 <input type="text" id="surname" name="soyisim" value={formInfo.soyisim} onChange={handleChange}></input>
                 </p>
                 <p>
                  <label>E-mail</label>
-                 <input type="text" id="email" name="email" value="email"></input>
+                 <input type="text" id="mail" name="email" value={formInfo.email} onChange={handleChange}></input>
                 </p>
                 <p>
                  <label>Şifre</label>
-                 <input type="password" id="password" name="password" value="password"></input>
+                 <input type="password" id="sifre" name="password" value={formInfo.password} onChange={handleChange}></input>
                 </p>
               </fieldset>
                 <p>
                 <label>Kullanım şartlarını kabul ediyor musunuz?</label><br />
                 <label>
                   <input
+                    onChange={handleChange}
                     type="checkbox"
                     name="kosulKabul"
-                    value="evet"
+                    value="yes"
+                    checked={formInfo.kosulKabul}
                  />
                     Evet
                 </label><br/>
                 <input type='button' value="Gönder"></input>
                 </p>
+                <button type="reset" onClick={handleReset}>Sil baştan başlamak gerek bazen</button>
             </form>
             
         </div>
